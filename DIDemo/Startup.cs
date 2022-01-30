669297bb981860e80f19784ca1c3de1ba0fa8462
Chris Ayers <clayers@gmail.com>
2022-01-30 10:22:52 -0500

    fix linting issues

--- DIDemo/Startup.cs
+++ DIDemo/Startup.cs
@@ -81,7 +81,7 @@ namespace DIDemo
             {
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
-           });
+            });
         }
     }
 }
